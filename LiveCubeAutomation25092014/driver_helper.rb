08a3c677eaddf14757e262c4c6344c49fd5ac91e
unknown <ankurp@360logica.com>
2014-09-29 17:59:05 +0530

    commited new driver helper

--- LiveCubeAutomation25092014/driver_helper.rb
+++ LiveCubeAutomation25092014/driver_helper.rb
@@ -1,5 +1,5 @@
 require "selenium-webdriver"
-require "test/unit"
+#require "test/unit"
 path = File.expand_path(File.dirname(__FILE__))
 Dir[path+"/support/**/*.rb"].each {|file| require file }
 require "json"
@@ -8,22 +8,7 @@ require "rspec"
 
 class DriverHelper
     
-    #function to set up testcase
-	def setup	
-		cc = ReadConfig.new()
-		browserType = cc.getApplication('BrowseType')
-		if browserType.eql?("ie")
-			@driver = Selenium::WebDriver.for :ie 
-		end
-		
-		if browserType.eql?("ff")
-			@driver = Selenium::WebDriver.for :firefox 
-		end
-		@base_url = cc.getApplication('URL')
-		@driver.manage.timeouts.implicit_wait = 30
-		@verification_errors = []
-	end
-	
+   
 	 def readAttendeesUrl()
     url=""
     f = File.open("attendeesUrl.txt", "r")
@@ -37,11 +22,6 @@ class DriverHelper
 	end
 
   
-    #function to close browse
-	def teardown	
-		@driver.quit
-		#assert_equal [], @verification_errors
-	end
-  
+   
     
 end
