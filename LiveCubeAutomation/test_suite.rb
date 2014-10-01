f021e0d353b4d1abf7932abdfde47d9b56be655d
nischalk <nischalk@360logica.com>
2014-10-01 19:33:08 +0530

    Testing

--- LiveCubeAutomation/test_suite.rb
+++ LiveCubeAutomation/test_suite.rb
@@ -1,7 +1,7 @@
 path = File.expand_path(File.dirname(__FILE__))
 #Dir[path+"/Scripts/*.rb"].each {|file| require file }
 require path+"/Scripts/TestCreateMeeting.rb"
-
+require path+"/Scripts/TestCreateSpeaker.rb"
 
 
 
